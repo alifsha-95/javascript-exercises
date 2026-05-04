d470146ad344c59a1dd96f9bb1d432421825bd5a
alifsha <alifshaedapt@gmail.com>
2026-05-04 06:58:01 +0000

    Complete exercises 06 and 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
+
+    const array = string.split('');
+    const reversed = array.reverse('');
+    const result = reversed.join('');
+
+    return result;
+    
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
