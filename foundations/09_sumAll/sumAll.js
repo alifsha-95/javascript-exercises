bdfab5e12fdd6ca4e66ce29cd031fadc82d0a00c
alifsha <alifshaedapt@gmail.com>
2026-05-05 09:20:00 +0000

    complet exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1 ,num2) {
+
+    const smaller = Math.min(num1,num2);
+    const larger = Math.max(num1,num2);
+   
+    if(!Number.isInteger(num1) || !Number.isInteger(num2) ) return 'ERROR' ;
+
+    //if inputs are negative return error.
+    if(num1 < 0  || num2 <0) return 'ERROR';
+
+     if (num1 > num2) {
+    const temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+
+
+     let sum = 0;
+    for(let i =smaller;i<=larger;i++){
+    
+        //adding each number to sum 
+         sum +=i;
+    }
+
+    return sum;
 
 };
+sumAll(1,4)
 
 // Do not edit below this line
 module.exports = sumAll;
