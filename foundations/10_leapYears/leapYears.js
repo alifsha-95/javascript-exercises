12a767eb3d859b0280ce0db47b0fdafe6f286df7
alifsha <alifshaedapt@gmail.com>
2026-05-05 09:39:20 +0000

    complet exercise 10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function (leapYear) {
+
+    const yearDivisibleByFour = leapYear % 4 === 0;
+    const century = leapYear % 100 === 0 ;
+    const divisibleByFourHundrend = leapYear % 400 === 0;
+
+    if(yearDivisibleByFour &&
+         (!century || divisibleByFourHundrend)){
+            return true;
+         }else 
+            return false;
+  
 
 };
+leapYears(2000);
 
 // Do not edit below this line
 module.exports = leapYears;
