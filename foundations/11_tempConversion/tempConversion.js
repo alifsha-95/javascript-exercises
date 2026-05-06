1cc9ef8f2a5be840029d3b75637b69d9a09bcb53
alifsha <alifshaedapt@gmail.com>
2026-05-06 06:04:32 +0000

    complete the exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const result = (fahrenheit -32)* 5/9 ;
+  return parseFloat(result.toFixed(1));
+  
+ 
 };
-
-const convertToFahrenheit = function() {
+convertToCelsius(100);
+const convertToFahrenheit = function(celsius) {
+  const result = (celsius*9/5+32);
+  return parseFloat(result.toFixed(1));
+ 
+  return celsius;
 };
+convertToFahrenheit(100);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
